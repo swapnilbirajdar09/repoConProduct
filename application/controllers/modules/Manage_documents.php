8f24f75987417adeb624e530566980f5eedb5ae7
being-samrat <mundesamrat@gmail.com>
2018-12-07 16:05:14 +0530

    commit: rest config

---
+++ application/controllers/modules/Manage_documents.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Manage_documents extends CI_Controller {
+
+    // Addproduct controller
+    public function __construct() {
+        parent::__construct();
+        // load common model
+        // $this->load->model('modules/product_model');
+    }
+
+    // main index function
+    public function index() {
+        // $this->load->view('includes/header');
+        // $this->load->view('pages/modules/manage_documents');
+        // $this->load->view('includes/footer');
+    }
+
+}
