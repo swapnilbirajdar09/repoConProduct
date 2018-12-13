8bfdb6af594e757d6f06b79bd5bedc92c261bd36
vaidehi <vaidehi.bizmotech@gmail.com>
2018-12-13 11:40:21 +0530

    commit

---
+++ application/controllers/user/Update_profile.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Update_profile extends CI_Controller {
+
+    // Addproduct controller
+    public function __construct() {
+        parent::__construct();
+        // load common model
+    }
+
+    // main index function
+    public function index() {
+     //   $data['user'] = User_settings::getUserDetails();
+        $this->load->view('includes/header');
+        $this->load->view('pages/user/update_profile');
+        $this->load->view('includes/footer');
+    }
